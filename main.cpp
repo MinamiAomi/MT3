9ea82675ae8a80328f08263a5da4dcdbd790f122
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-04-12 01:31:19 +0900

    確認課題5

--- main.cpp
+++ main.cpp
@@ -14,13 +14,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, (int)kWindowWidth, (int)kWindowHeight);
 	Input* input = Input::GetInstance();
 
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateXYZMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
-
-	constexpr int kRowHeight{ 20 };
+	Vector3 scale{ 1.2f, 0.79f, -2.1f };
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Vector3 translate{ 2.7f, -4.15f, 1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -41,10 +38,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		
-		MatrixScreenPrintf(0, 0, rotateXMatrix, "RotateXMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix, "RotateYMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix, "RotateZMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "RotateXYZMatrix");
+		MatrixScreenPrintf(0, 0, worldMatrix, "WorldMatrix");
 		
 
 		///
